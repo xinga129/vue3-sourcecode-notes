85e8def2e68568059b77a60076f8c9dbf5b14d99
李甜鑫 <litianxin@rhtect.com>
2024-03-15 11:46:56 +0800

    reactive API: getter\setter

--- README.md
+++ README.md
@@ -4,26 +4,6 @@
 
 Please follow the documentation at [vuejs.org](https://vuejs.org/)!
 
-## Sponsors
-
-Vue.js is an MIT-licensed open source project with its ongoing development made possible entirely by the support of these awesome [backers](https://github.com/vuejs/core/blob/main/BACKERS.md). If you'd like to join them, please consider [ sponsoring Vue's development](https://vuejs.org/sponsor/).
-
-<p align="center">
-  <h3 align="center">Special Sponsor</h3>
-</p>
-
-<p align="center">
-  <a target="_blank" href="https://github.com/appwrite/appwrite">
-  <img alt="special sponsor appwrite" src="https://sponsors.vuejs.org/images/appwrite.svg" width="300">
-  </a>
-</p>
-
-<p align="center">
-  <a target="_blank" href="https://vuejs.org/sponsor/#current-sponsors">
-    <img alt="sponsors" src="https://sponsors.vuejs.org/sponsors.svg?v3">
-  </a>
-</p>
-
 ## Questions
 
 For questions and support please use [the official forum](https://forum.vuejs.org) or [community chat](https://chat.vuejs.org/). The issue list of this repo is **exclusively** for bug reports and feature requests.
@@ -37,17 +17,3 @@ Please make sure to respect issue requirements and use [the new issue helper](ht
 - [Twitter](https://twitter.com/vuejs)
 - [Blog](https://blog.vuejs.org/)
 - [Job Board](https://vuejobs.com/?ref=vuejs)
-
-## Contribution
-
-Please make sure to read the [Contributing Guide](https://github.com/vuejs/core/blob/main/.github/contributing.md) before making a pull request. If you have a Vue-related project/component/tool, add it with a pull request to [this curated list](https://github.com/vuejs/awesome-vue)!
-
-Thank you to all the people who already contributed to Vue!
-
-<a href="https://github.com/vuejs/core/graphs/contributors"><img src="https://opencollective.com/vuejs/contributors.svg?width=890" /></a>
-
-## License
-
-[MIT](https://opensource.org/licenses/MIT)
-
-Copyright (c) 2013-present, Yuxi (Evan) You
--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -45,20 +45,31 @@ const builtInSymbols = new Set(
 
 const arrayInstrumentations = /*#__PURE__*/ createArrayInstrumentations()
 
+// arrayInstrumentations 函数重写了数组中的 includes， indexOf， lastIndexOf 函数
+// 当 target 是一个数组的时候, 我们执行函数 target.i∩c1udes, target.i∩dexOf 或者 target.lastIndexOf 就会被代理到这些重写的函数
+
+// todo: 
+// 除了调用数组本身的函数求值,还会对数组的每个元素做依赖收集
+// 因为数组的元素一旦被修改, 这几个API的返回结果都可能发生变化’所以我们需要跟踪数组每个元素的变化
 function createArrayInstrumentations() {
   const instrumentations: Record<string, Function> = {}
   // instrument identity-sensitive Array methods to account for possible reactive
   // values
   ;(['includes', 'indexOf', 'lastIndexOf'] as const).forEach(key => {
     instrumentations[key] = function (this: unknown[], ...args: unknown[]) {
+
+      // toRaw可以把响应式对象转成原始数据
       const arr = toRaw(this) as any
       for (let i = 0, l = this.length; i < l; i++) {
+        // 依赖收集
         track(arr, TrackOpTypes.GET, i + '')
       }
       // we run the method using the original args first (which may be reactive)
+      // 先尝试使用参数本身，它可能是响应式数据
       const res = arr[key](...args)
       if (res === -1 || res === false) {
         // if that didn't work, run it again using raw values.
+        // 如果失败，再尝试把参数转换成原始数据
         return arr[key](...args.map(toRaw))
       } else {
         return res
@@ -92,16 +103,24 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
     protected readonly _isShallow = false,
   ) {}
 
+  // 依赖收集发生在数据访问阶段
+  // get 函数的实现: 它是执行 返回值,其中 isReadonly 默认为 false
   get(target: Target, key: string | symbol, receiver: object) {
     const isReadonly = this._isReadonly,
       isShallow = this._isShallow
+
+    // （1） 对特殊的key做了代理
     if (key === ReactiveFlags.IS_REACTIVE) {
+      // 代理 proxy.__v_isReactive
       return !isReadonly
     } else if (key === ReactiveFlags.IS_READONLY) {
+      // 代理 
       return isReadonly
     } else if (key === ReactiveFlags.IS_SHALLOW) {
+      // 代理 ..
       return isShallow
     } else if (key === ReactiveFlags.RAW) {
+      // 代理 
       if (
         receiver ===
           (isReadonly
@@ -124,6 +143,8 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
 
     const targetIsArray = isArray(target)
 
+    // (2) 如果target是数组且 key 命中了 arrayInstrumentations, 则执行其内部对应的函数
+    // arrayInstrumentations 包含对数组一些函数修改的函数
     if (!isReadonly) {
       if (targetIsArray && hasOwn(arrayInstrumentations, key)) {
         return Reflect.get(arrayInstrumentations, key, receiver)
@@ -132,13 +153,15 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
         return hasOwnProperty
       }
     }
-
+    // 求值
     const res = Reflect.get(target, key, receiver)
 
+    // 内置 Symbol key， 不需要依赖收集
     if (isSymbol(key) ? builtInSymbols.has(key) : isNonTrackableKeys(key)) {
       return res
     }
 
+    // 依赖收集
     if (!isReadonly) {
       track(target, TrackOpTypes.GET, key)
     }
@@ -156,6 +179,10 @@ class BaseReactiveHandler implements ProxyHandler<Target> {
       // Convert returned value into a proxy as well. we do the isObject check
       // here to avoid invalid value warning. Also need to lazy access readonly
       // and reactive here to avoid circular dependency.
+
+      // 如果 res 是对象或者数组类型的, 则递归执行 reactive 函数,把 res 变成响应式的
+
+      // 这么做是因为 oroxy 劫持的是对象本身, 并不能劫持子对象的变化。这点与 Object.defineProperty 一致
       return isReadonly ? readonly(res) : reactive(res)
     }
 
